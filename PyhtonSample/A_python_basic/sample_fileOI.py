60f47da70aba70339d5dd11ba3435ad5153ae3c0
Kai3645 <kaisgomail@gmail.com>
2020-12-18 10:08:03 +0900

    Update sample_fileOI.py

--- PyhtonSample/A_python_basic/sample_fileOI.py
+++ PyhtonSample/A_python_basic/sample_fileOI.py
@@ -1,7 +1,5 @@
 import csv
 import json
-import os
-import time
 
 
 def csv_reader(path, hasHead = True):
@@ -21,51 +19,51 @@ def json_reader(path):
 	pass
 
 
-def file_length_(path, rep = 1):
-	print("func 01:")  # sys memory killer
-	t0 = time.time()
-	for _ in range(rep):
-		fr = open(path, "r")
-		count = len(fr.readlines())
-		print(count)
-		fr.close()
-	print("time = " + str(time.time() - t0))
-	print()
-	
-	print("func 02:")
-	t0 = time.time()
-	for _ in range(rep):
-		fr = open(path, "r")
-		count = 0
-		for _ in fr.readlines(): count += 1
-		print(count)
-		fr.close()
-	print("time = " + str(time.time() - t0))
-	print()
-	
-	print("func 03:")  # best
-	t0 = time.time()
-	for _ in range(rep):
-		fr = open(path, "r")
-		count = -1
-		for count, _ in enumerate(fr): pass
-		count += 1
-		print(count)
-		fr.close()
-	print("time = " + str(time.time() - t0))
-	print()
+# def file_length_(path, rep = 1):
+# 	print("func 01:")  # sys memory killer
+# 	t0 = time.time()
+# 	for _ in range(rep):
+# 		fr = open(path, "r")
+# 		count = len(fr.readlines())
+# 		print(count)
+# 		fr.close()
+# 	print("time = " + str(time.time() - t0))
+# 	print()
+#
+# 	print("func 02:")
+# 	t0 = time.time()
+# 	for _ in range(rep):
+# 		fr = open(path, "r")
+# 		count = 0
+# 		for _ in fr.readlines(): count += 1
+# 		print(count)
+# 		fr.close()
+# 	print("time = " + str(time.time() - t0))
+# 	print()
+#
+# 	print("func 03:")  # best
+# 	t0 = time.time()
+# 	for _ in range(rep):
+# 		fr = open(path, "r")
+# 		count = -1
+# 		for count, _ in enumerate(fr): pass
+# 		count += 1
+# 		print(count)
+# 		fr.close()
+# 	print("time = " + str(time.time() - t0))
+# 	print()
 
 
-def csv_file_length(path, rep = 1):
-	file_length(path, rep)
-	
-	print("func 04:")
-	t0 = time.time()
-	for _ in range(rep):
-		fr_csv = open(path, "r")
-		csv_rows = csv.reader(fr_csv)
-		count = len(list(csv_rows)) - 1
-		print(count)
-		fr_csv.close()
-	print("time = " + str(time.time() - t0))
-	print()
+# def csv_file_length(path, rep = 1):
+# 	file_length(path, rep)
+#
+# 	print("func 04:")
+# 	t0 = time.time()
+# 	for _ in range(rep):
+# 		fr_csv = open(path, "r")
+# 		csv_rows = csv.reader(fr_csv)
+# 		count = len(list(csv_rows)) - 1
+# 		print(count)
+# 		fr_csv.close()
+# 	print("time = " + str(time.time() - t0))
+# 	print()
