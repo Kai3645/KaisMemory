64d3a0c549aba70c5aa485db956c9c7cef5c6f1c
Kai3645 <33736332+Kai3645@users.noreply.github.com>
2021-05-06 11:51:54 +0900

    update

--- PyhtonSample/stereo_camera/recorder.py
+++ PyhtonSample/stereo_camera/recorder.py
@@ -5,14 +5,19 @@ import cv2
 import numpy as np
 
 from Core.Basic import mkdir
+from Core.ComputerVision.camera_basic import chessboard_corner_detect, chessboard_draw_corner, chessboard_area_ratio
+from Core.ComputerVision.image_basic import gray2binary
 
 
 def main():
 	folder_L = mkdir(folder, "LCam")
 	folder_R = mkdir(folder, "RCam")
 
-	cap_L = cv2.VideoCapture(3)
-	cap_R = cv2.VideoCapture(0)
+	board_shape = (8, 5)
+	image_shape = (1280, 720)
+
+	cap_L = cv2.VideoCapture(0)
+	cap_R = cv2.VideoCapture(3)
 
 	if cap_L.isOpened() and cap_R.isOpened():
 		print(">> cameras start ..")
@@ -20,33 +25,43 @@ def main():
 		print(">> err, can not open camera ..")
 		exit(-1)
 
-	value_L = np.empty(2, object)
-	value_R = np.empty(2, object)
-	display = np.zeros((144, 256), np.uint8)
-	is_recording = False
+	value_L = np.empty(3, object)
+	value_R = np.empty(3, object)
+	display = np.zeros((720, 640, 3), np.uint8)
 
-	def func_L(i):
+	def process(img):
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		binary = gray2binary(gray)
+		corners = chessboard_corner_detect(gray, board_shape, binary)
+		if corners is None: return False, img
+
+		area_ratio = chessboard_area_ratio(corners, board_shape, image_shape)
+		if area_ratio < 0.15 or area_ratio > 0.40:
+			return False, chessboard_draw_corner(img, corners, cname = "cool")
+		return True, chessboard_draw_corner(img, corners, cname = "hsv")
+
+	def func_L():
+		dst = None
 		ret, img = cap_L.read()
 		if ret:
 			img = img[::-1, ::-1]
-			if is_recording:
-				cv2.imwrite(folder_L + f"L{i:04d}.jpg", img)
-		value_L[:] = ret, img
+			ret, dst = process(img)
+		value_L[:] = ret, dst, img
 
-	def func_R(i):
+	def func_R():
+		dst = None
 		ret, img = cap_R.read()
 		if ret:
 			img = img[::-1, ::-1]
-			if is_recording:
-				cv2.imwrite(folder_R + f"R{i:04d}.jpg", img)
-		value_R[:] = ret, img
+			ret, dst = process(img)
+		value_R[:] = ret, dst, img
 
-	loop = 1
-	count = 0
+	loop, count = 0, 0
 	t0 = time.time()
 	while True:
-		staff_L = Thread(target = func_L, args = (count,))
-		staff_R = Thread(target = func_R, args = (count,))
+		display[:, :, :] = 0
+		staff_L = Thread(target = func_L)
+		staff_R = Thread(target = func_R)
 
 		staff_L.start()
 		staff_R.start()
@@ -54,26 +69,23 @@ def main():
 		staff_R.join()
 
 		if value_L[0] and value_R[0]:
-			img_L = value_L[1][::5, ::5]
-			grayL = cv2.cvtColor(img_L, cv2.COLOR_BGR2GRAY)
-			img_R = value_R[1][::5, ::5]
-			grayR = cv2.cvtColor(img_R, cv2.COLOR_BGR2GRAY)
-
-			display[:, :] = np.abs(grayL.astype(int) - grayR)
-			cv2.imshow("cam", display)
-		else: print(">> camera short circuit")
+			print(count)
+			cv2.imwrite(folder_L + f"L{count:06d}.jpg", value_L[2])
+			cv2.imwrite(folder_R + f"R{count:06d}.jpg", value_R[2])
+			count += 1
+		if value_L[1] is not None:
+			display[:360, :, :] = value_L[1][::2, ::2, :]
+		if value_R[1] is not None:
+			display[360:, :, :] = value_R[1][::2, ::2, :]
+		cv2.imshow("cam", display)
 
 		key = cv2.waitKey(1)
 		if key == 27: break
-		if key == ord("s"):
-			print(">> keyboard -> s")
-			is_recording = not is_recording
-		if is_recording: count += 1
 
+		loop += 1
 		dt = time.time() - t0
 		fps = loop / dt
-		print(f"({count}, {dt:.3f}), fps = {fps:.1f}")
-		loop += 1
+		print(f"{loop}, {fps:.1f}, {value_L[0]}, {value_R[0]}, {count}")
 	print(">> process finished ..")
 
 
--- PyhtonSample/web_cam/test.py
+++ PyhtonSample/web_cam/test.py
@@ -2,7 +2,7 @@ import cv2
 
 
 def main():
-	cap = cv2.VideoCapture(2)
+	cap = cv2.VideoCapture(3)
 
 	# cv2.namedWindow("cam", cv2.WINDOW_AUTOSIZE)
 	# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
