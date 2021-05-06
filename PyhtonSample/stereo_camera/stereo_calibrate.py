de5358bb175baf23290be4a39f20bbe608d96fbb
Kai3645 <kaisgomail@gmail.com>
2021-05-07 07:26:17 +0900

    update

---
+++ PyhtonSample/stereo_camera/stereo_calibrate.py
@@ -0,0 +1,139 @@
+import time
+
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+from Core.Basic import listdir, err_exit, num2str, mkdir
+from Core.ComputerVision.camera_basic import chessboard_corner_3d, chessboard_corner_detect
+from Core.ComputerVision.image_basic import gray2binary
+
+if __name__ == '__main__':
+	folder_out = "/home/kai/PycharmProjects/pyCenter/diary_output/d_20210507/"
+	BOARD_R = 0.04231
+	BOARD_W = 8
+	BOARD_H = 5
+	IMAGE_W = 1280
+	IMAGE_H = 720
+	board_shape = (BOARD_W, BOARD_H)
+	image_shape = (IMAGE_W, IMAGE_H)
+	CHANGE_RATIO = 0.1
+	change_th = IMAGE_W * IMAGE_H * CHANGE_RATIO
+	do_rotate_image = True
+
+	mat_L = np.asarray([373.2465, 0, 626.9151, 0, 373.3987, 363.1092, 0, 0, 1]).reshape((3, 3))
+	mat_R = np.asarray([370.7673, 0, 603.3985, 0, 370.6573, 349.1074, 0, 0, 1]).reshape((3, 3))
+	dist_L = np.asarray([2.50764566, 3.69586221, 0.00208109, 0.00235887,
+	                     0.05245894, 2.22065863, 3.17493684, 0.34092975,
+	                     -0.00070810, -0.00030914, -0.00230941, -0.00007401,
+	                     -0.00423250, 0.00819843])
+	dist_R = np.asarray([2.85760726, 4.60387629, 0.00014143, 0.00221237,
+	                     0.06523993, 2.58364108, 3.89364134, 0.42966194,
+	                     -0.00247370, 0.00001714, -0.00042481, -0.00004474,
+	                     0.00310306, 0.00342365])
+
+	time_start = time.time()
+
+	# collect images paths
+	imgs_path_L = "/mnt/DATA_SSD/Sample_Dataset/stereo_camera/LCam/"
+	imgs_path_R = "/mnt/DATA_SSD/Sample_Dataset/stereo_camera/RCam/"
+	img_names_L = listdir(imgs_path_L)
+	img_names_R = listdir(imgs_path_R)
+
+
+	def corner_det(path, binary_old):
+		img = cv2.imread(path)
+		if do_rotate_image: img = img[::-1, ::-1]
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		binary = gray2binary(gray)
+
+		diff_valid = binary_old != binary
+		if np.sum(diff_valid) < change_th: return False, binary_old, None
+
+		corners = chessboard_corner_detect(gray, board_shape, binary)
+		if corners is None: return False, binary_old, None
+		return True, binary, corners
+
+
+	img_corners_L, img_corners_R = [], []
+	binary_old_l = np.zeros((IMAGE_H, IMAGE_W), np.uint8)
+	binary_old_r = np.zeros((IMAGE_H, IMAGE_W), np.uint8)
+
+	for n_l, n_r in tqdm(zip(img_names_L, img_names_R), total = len(img_names_L), desc = ">> scanning "):
+		if n_l[1:] != n_r[1:]: err_exit(n_l + " =/= " + n_r + ", name match err ..")
+
+		flag, binary_l, corners_l = corner_det(imgs_path_L + n_l, binary_old_l)
+		if not flag: continue
+		flag, binary_r, corners_r = corner_det(imgs_path_R + n_r, binary_old_r)
+		if not flag: continue
+
+		img_corners_L.append(corners_l)
+		img_corners_R.append(corners_r)
+		binary_old_l = binary_l
+		binary_old_r = binary_r
+	img_corners_L = np.asarray(img_corners_L)
+	img_corners_R = np.asarray(img_corners_R)
+
+	test_num = -1
+	CALIB_MAX_ABILITY = 250
+	CALIB_MIN_IMAGE_NUM = 40
+	if test_num < 0: test_num = CALIB_MAX_ABILITY
+	else: test_num = min(test_num, CALIB_MAX_ABILITY)
+
+	working_corners_L = img_corners_L
+	working_corners_R = img_corners_R
+	frame_count = len(img_corners_L)
+	if frame_count < CALIB_MIN_IMAGE_NUM:
+		err_exit(">> err, need more good chessboard images .. ")
+	if frame_count > test_num:
+		indexes = np.arange(test_num) / test_num
+		indexes = (indexes * frame_count).astype(int)
+		working_corners_L = img_corners_L[indexes]
+		working_corners_R = img_corners_R[indexes]
+		frame_count = test_num
+
+	corners_3d = chessboard_corner_3d(BOARD_W, BOARD_H, BOARD_R)
+	object_points = np.tile(corners_3d, (frame_count, 1, 1))
+
+	_, mat_L, dist_L, mat_R, dist_R, R, T, _, _ = cv2.stereoCalibrate(
+		object_points, working_corners_L, working_corners_R,
+		mat_L, dist_L, mat_R, dist_R, image_shape, flags = (
+			cv2.CALIB_USE_INTRINSIC_GUESS |
+			cv2.CALIB_RATIONAL_MODEL |
+			cv2.CALIB_TILTED_MODEL |
+			cv2.CALIB_THIN_PRISM_MODEL
+		)
+	)
+
+	fw = open(folder_out + "stereo_calib_result.txt", "w")
+	fw.write("L distCoeffs = \n\t" + num2str(dist_L, 8) + "\n")
+	fw.write("L camera mat = \n\t" + num2str(mat_L, 4) + "\n")
+	fw.write("R distCoeffs = \n\t" + num2str(dist_R, 8) + "\n")
+	fw.write("R camera mat = \n\t" + num2str(mat_L, 4) + "\n")
+	fw.write("R = \n\t" + num2str(R, 8) + "\n")
+	fw.write("T = \n\t" + num2str(T, 4) + "\n")
+	fw.close()
+
+	print("===============================")
+	print("R = ")
+	print(R)
+	print("- - - - - - - - - - - - - - - -")
+	print("T = ")
+	print(T)
+	print("===============================")
+
+	R1, R2, P1, P2, Q, _, _ = cv2.stereoRectify(mat_L, dist_L, mat_R, dist_R, image_shape, R, T, alpha = 1)
+	map1_1, map1_2 = cv2.initUndistortRectifyMap(mat_L, dist_L, R1, P1, image_shape, cv2.CV_16SC2)
+	map2_1, map2_2 = cv2.initUndistortRectifyMap(mat_R, dist_R, R2, P2, image_shape, cv2.CV_16SC2)
+
+	folder_stereo = mkdir(folder_out, "stereo")
+	for i in tqdm(range(len(img_names_L)), desc = ">> undistort "):
+		img1 = cv2.imread(imgs_path_L + img_names_L[i])[::-1, ::-1]
+		img2 = cv2.imread(imgs_path_R + img_names_R[i])[::-1, ::-1]
+		result1 = cv2.remap(img1, map1_1, map1_2, cv2.INTER_CUBIC)
+		result2 = cv2.remap(img2, map2_1, map2_2, cv2.INTER_CUBIC)
+		result = np.concatenate((result1[:, ::3], result2[:, ::3]), axis = 1)
+		result[13::27, :, 1] = 150
+		cv2.imwrite(folder_stereo + f"{i:05d}.jpg", result)
+	time_used = time.time() - time_start
+	print(f">> process finished, time used = {time_used:.2f} s .. ")
--- PyhtonSample/stereo_camera/viewer.py
+++ PyhtonSample/stereo_camera/viewer.py
@@ -1,17 +1,32 @@
-import threading
 import time
+from threading import Thread
 
 import cv2
 import numpy as np
 
 
 def main():
+	mat_L = np.asarray([372.4407, 0, 626.7559, 0, 372.5443, 362.9187, 0, 0, 1]).reshape([3, 3])
+	mat_R = np.asarray([372.4407, 0, 626.7559, 0, 372.5443, 362.9187, 0, 0, 1]).reshape([3, 3])
+	dist_L = np.asarray([2.85419397, 4.41270687, 0.00016594, 0.00290789, 0.07042960, 2.57469084,
+	                     3.74501496, 0.42086447, -0.00147710, -0.00023769, -0.00066186, 0.00004257,
+	                     0.00026240, 0.01072015])
+	dist_R = np.asarray([2.58339740, 4.27790321, -0.00000468, 0.00133856, 0.04181726, 2.31375616,
+	                     3.64480336, 0.37388115, -0.00173613, 0.00014957, -0.00010390, -0.00005456,
+	                     0.00259686, 0.00150627])
+	R = np.asarray([0.99990214, -0.00524353, -0.01297007,
+	                0.00522852, 0.99998562, -0.00119088,
+	                0.01297612, 0.00112295, 0.99991518]).reshape([3, 3])
+	T = np.asarray([0.0623, 0.0001, 0.0011]).reshape([3, 1])
+
+	image_shape = (1280, 720)
+
+	R1, R2, P1, P2, Q, _, _ = cv2.stereoRectify(mat_L, dist_L, mat_R, dist_R, image_shape, R, T, alpha = 0)
+	map1_1, map1_2 = cv2.initUndistortRectifyMap(mat_L, dist_L, R1, P1, image_shape, cv2.CV_16SC2)
+	map2_1, map2_2 = cv2.initUndistortRectifyMap(mat_R, dist_R, R2, P2, image_shape, cv2.CV_16SC2)
+
 	cap_L = cv2.VideoCapture(0)
 	cap_R = cv2.VideoCapture(3)
-	# cap_L.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-	# cap_R.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-	# cap_L.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-	# cap_R.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 	if cap_L.isOpened() and cap_R.isOpened():
 		print(">> cameras start ..")
@@ -19,81 +34,54 @@ def main():
 		print(">> err, can not open camera ..")
 		exit(-1)
 
-	display_W = 640
-	display_H = 360
-	display = np.zeros((display_H * 3, display_W * 3, 3), np.uint8)
-
-	def multi_display(images):
-		for i, src in enumerate(images):
-			if src is None: continue
-			dst = cv2.resize(src, (display_W, display_H))
-			if len(dst.shape) < 3:
-				dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
-			y, x = i // 3 * display_H, i % 3 * display_W
-			display[y:y + display_H, x:x + display_W, :] = dst[::-1, ::-1]
-		return display
-
 	value_L = np.empty(2, object)
 	value_R = np.empty(2, object)
-	is_running = True
-	imgs = np.empty(9, object)
-	thread_lock = threading.Lock()
 
-	class StaffL(threading.Thread):
-		def run(self):
-			value_L[:] = ret, img = cap_L.read()
-			if ret:
-				gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-				gray = cv2.GaussianBlur(gray, (11, 11), 10)
-				thread_lock.acquire()
-				imgs[0] = img
-				imgs[1] = gray
-				thread_lock.release()
+	mix_image = False
 
-		pass
+	def func_L():
+		ret, img = cap_L.read()
+		if ret:
+			img = cv2.remap(img[::-1, ::-1], map1_1, map1_2, cv2.INTER_LINEAR_EXACT)
+			if mix_image: img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			else: img = cv2.resize(img, (640, 720), interpolation = cv2.INTER_CUBIC)
+		value_L[:] = ret, img
 
-	class StaffR(threading.Thread):
-		def run(self):
-			value_R[:] = ret, img = cap_R.read()
-			if ret:
-				gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-				gray = cv2.GaussianBlur(gray, (11, 11), 10)
-				thread_lock.acquire()
-				imgs[3] = img
-				imgs[4] = gray
-				thread_lock.release()
+	def func_R():
+		ret, img = cap_R.read()
+		if ret:
+			img = cv2.remap(img[::-1, ::-1], map2_1, map2_2, cv2.INTER_LINEAR_EXACT)
+			if mix_image: img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			else: img = cv2.resize(img, (640, 720), interpolation = cv2.INTER_CUBIC)
+		value_R[:] = ret, img
 
-		pass
-
+	loop = 1
 	t0 = time.time()
-	count = 1
-	while is_running:
-		imgs = np.empty(9, object)
-
-		staff_L = StaffL()
-		staff_R = StaffR()
+	while True:
+		staff_L = Thread(target = func_L)
+		staff_R = Thread(target = func_R)
 		staff_L.start()
 		staff_R.start()
 		staff_L.join()
 		staff_R.join()
 
 		if value_L[0] and value_R[0]:
-			imgs[6] = (imgs[0] / 2 + imgs[3] / 2).astype(np.uint8)
-			imgs[7] = (imgs[1] / 2 + imgs[4] / 2).astype(np.uint8)
-			imgs[8] = np.abs(np.asarray(imgs[1], int) - imgs[4]).astype(np.uint8)
-			imgs[8][imgs[8] < 50] = 0
-
-			cv2.imshow("cam", multi_display(imgs))
-			if cv2.waitKey(1) == 27:
-				is_running = False
-				print(">> exit ..")
+			img_l = value_L[1]
+			img_r = value_R[1]
+			if mix_image:
+				result = np.abs(img_l.astype(int) - img_r).astype(np.uint8)
+			else:
+				result = np.concatenate((img_l, img_r), axis = 1)
+			cv2.imshow("cam", result)
 		else: print(">> camera short circuit")
-
-		dt = time.time() - t0
-		fps = count / dt
-		print(f"({count}, {dt:.3f}), fps = {fps:.1f}")
-		count += 1
+		key = cv2.waitKey(1)
+		if key == 27: break
+		if key == "p": mix_image = not mix_image
+		fps = loop / (time.time() - t0)
+		print(f"{loop}, fps = {fps:.1f}")
+		loop += 1
 	print(">> process finished ..")
+	pass
 
 
 if __name__ == '__main__':
