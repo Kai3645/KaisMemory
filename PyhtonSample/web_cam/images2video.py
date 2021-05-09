f572691f2e3476f47c6f9dbfebf0ec7be1bd530e
Kai3645 <33736332+Kai3645@users.noreply.github.com>
2021-05-09 15:03:31 +0900

    update

--- PyhtonSample/web_cam/images2video.py
+++ PyhtonSample/web_cam/images2video.py
@@ -4,14 +4,14 @@ from tqdm import tqdm
 from Core.Basic import listdir, str2folder
 
 if __name__ == '__main__':
-	folder = str2folder("/Users/kaismac/Downloads/time_lapse")
+	folder = str2folder("/Users/kaismac/Downloads/TimeLapse")
 
 	names = listdir(folder, pattern = "*.jpg")
 	out = cv2.VideoWriter(
-		'/Users/kaismac/Downloads/out.mov', apiPreference = cv2.CAP_ANY,
-		fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps = 60, frameSize = (1200, 675),
+		folder + 'out.mov', apiPreference = cv2.CAP_ANY,
+		fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps = 30, frameSize = (1200, 675),
 	)  # 861030210
-	for n in tqdm(names[300:]):
+	for n in tqdm(names):
 		img = cv2.imread(folder + n)
 		out.write(img)
 	pass
--- PyhtonSample/web_cam/time_lapse.py
+++ PyhtonSample/web_cam/time_lapse.py
@@ -1,51 +1,86 @@
-import time
-
-import cv2
-
-from Core.Basic import mkdir
-
-
-def main():
-	folder = mkdir("/Users/kaismac/Downloads", "time_lapse")
-
-	cap = cv2.VideoCapture(2)
-	if not cap.isOpened():
-		print(">> err, can not open camera ..")
-		exit(-1)
-
-	loop = 1
-	FPS_SET = 1
-	DT_SET = 1 / FPS_SET
-	delay = 1000 / FPS_SET - 60
-
-	t0 = ti = time.time()
-	while True:
-		ret, img = cap.read()
-		if ret:
-			img = cv2.GaussianBlur(img, (3, 3), 1.4)
-			info = time.strftime("%y.%m.%d %H:%M:%S", time.localtime())
-			cv2.putText(img, info, (20, 42), cv2.FONT_HERSHEY_DUPLEX, 0.9, [200, 200, 200], 6)
-			cv2.putText(img, info, (20, 42), cv2.FONT_HERSHEY_DUPLEX, 0.9, [175, 175, 175], 3)
-			cv2.putText(img, info, (20, 42), cv2.FONT_HERSHEY_DUPLEX, 0.9, [85, 85, 85], 2)
-			cv2.putText(img, info, (20, 42), cv2.FONT_HERSHEY_DUPLEX, 0.9, [0, 0, 0], 1)
-			img = cv2.resize(img, (1200, 675), interpolation = cv2.INTER_CUBIC)
-			cv2.imwrite(folder + f"T{loop:05d}.jpg", img)
-			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-			cv2.imshow(folder + f"cam", gray[::2, ::2])
-		else:
-			print(">> camera short circuit")
-			break
-		if cv2.waitKey(int(round(delay))) == 27: break
-
-		t_ = ti
-		ti = time.time()
-		fps = loop / (ti - t0)
-		delay -= 20 * (ti - t_ - DT_SET)
-		print(f"{loop:06d}, fps = {fps:.3f}, delay = {delay:.0f}")
-		loop += 1
-	print(">> finished ..")
-
-
-if __name__ == '__main__':
-	main()
-	pass
+# import sys
+# import time
+#
+# import cv2
+# import numpy as np
+#
+#
+# def main():
+# 	folder = mkdir("/Users/kaismac/Downloads", "time_lapse")
+#
+# 	cap = cv2.VideoCapture(2)
+# 	if not cap.isOpened():
+# 		print(">> err, can not open camera ..")
+# 		exit(-1)
+#
+# 	loop = 1
+# 	FPS_SET = 1
+# 	DT_SET = 1 / FPS_SET
+#
+# 	sys_delay = 60
+# 	delay = 1000 / FPS_SET - sys_delay
+#
+# 	fps = FPS_SET
+# 	text_max = 0
+# 	ti = time.time()
+# 	print(">> start ..")
+# 	while True:
+# 		print("\r" * text_max, end = "")
+# 		ret, img = cap.read()
+# 		if ret:
+# 			img = cv2.GaussianBlur(img, (3, 3), 1.4)
+#
+# 			mask = np.zeros((160, 1100), np.uint8)
+# 			info = time.strftime("%y.%m.%d %H:%M:%S", time.localtime())
+# 			cv2.putText(mask, info, (23, 120), cv2.FONT_HERSHEY_DUPLEX, 3.5, 50, 15)
+# 			cv2.putText(mask, info, (23, 120), cv2.FONT_HERSHEY_DUPLEX, 3.5, 240, 2)
+# 			kernel = np.ones((5, 5), np.float32) / 25
+# 			mask = cv2.filter2D(mask, -1, kernel)
+#
+# 			w, h, off1, off2 = 240, 44, 18, 13
+# 			mask = cv2.resize(mask, (w, h), interpolation = cv2.INTER_CUBIC)
+# 			mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+# 			sub_img = img[off2:h + off2, off1:w + off1].astype(int)
+# 			ave = np.average(sub_img)
+# 			if ave < 100:
+# 				sub_img += mask
+# 				sub_img[sub_img > 255] = 255
+# 			elif ave < 160:
+# 				valid = mask > 0
+# 				sub_img[valid] = sub_img[valid] * 0.3 + mask[valid] * 1.2
+# 				sub_img[sub_img > 255] = 255
+# 			else:
+# 				sub_img -= mask
+# 				sub_img[sub_img < 0] = 0
+# 			img[off2:h + off2, off1:w + off1, :] = sub_img.astype(np.uint8)
+#
+# 			img = cv2.resize(img, (1200, 675), interpolation = cv2.INTER_CUBIC)
+# 			cv2.imwrite(folder + f"T{loop:05d}.jpg", img)
+# 			sub_img = cv2.GaussianBlur(img, (3, 3), 1.22)
+# 			sub_img = cv2.resize(sub_img, (800, 450), interpolation = cv2.INTER_CUBIC)
+# 			cv2.imshow("camera", sub_img)
+# 		else: print(">> camera short circuit")
+# 		if cv2.waitKey(int(round(delay))) == 27: break
+#
+# 		t_ = ti
+# 		ti = time.time()
+# 		dt = ti - t_
+# 		delay -= 20 * (1 / fps - DT_SET)
+# 		fps = fps * 0.4 + 0.6 / dt
+# 		info = f"{loop:06d}, fps = {fps:.3f}, delay = {delay:.0f}"
+#
+# 		info_len = len(info)
+# 		if info_len < text_max: info += " " * (text_max - info_len)
+# 		else: text_max = info_len
+# 		print(info, end = "")
+# 		loop += 1
+# 	print()
+# 	print(">> finished ..")
+#
+#
+# if __name__ == '__main__':
+# 	sys.path.append("/Users/kaismac/Documents/GitHub/ACRS_MMS/Python")
+# 	from Core.Basic import mkdir
+#
+# 	main()
+# 	pass
